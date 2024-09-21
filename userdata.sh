9f9275ea70bbdfded33b734643e9233e1e44a678
Deepak Sharma <64480480+deepaksharma2007@users.noreply.github.com>
2024-09-21 19:49:11 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-yum update -y
-yum install -y httpd.x86_64
-systemctl start httpd
-systemctl enable httpd
+sudo yum update -y
+sudo yum install -y httpd.x86_64
+sudo systemctl start httpd
+sudo systemctl enable httpd
 echo "Welcome to the Deepak Webserver $(hostname -f)" > /var/www/html/index.html
